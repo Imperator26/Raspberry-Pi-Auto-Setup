c0d1fe013187b9d81e2c19b7bffded3ff1862a36
SuperHelios <20555025+Imperator26@users.noreply.github.com>
2020-02-25 10:15:53 +0100

    Create rtl-sdr setup script

---
+++ rtl-sdr.sh
@@ -0,0 +1,12 @@
+sudo apt-get install -y git cmake pkg-config libusb-1.0-0-dev
+git clone git://git.osmocom.org/rtl-sdr.git
+cd rtl-sdr
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo ldconfig
+echo "blacklist dvb_usb_rtl28xxu\nblacklist rtl2832\nblacklist rtl2830" | sudo tee -a /etc/modprobe.d/blacklist-rtl.conf
+echo "Reboot to complete the installation."
